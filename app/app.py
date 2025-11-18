70cf09e6125fcf34386fea63ca928dbfce068bac
houimliraed <houimliraed@outlook.fr>
2025-11-18 21:16:42 +0100

    added lifespan

--- app/app.py
+++ app/app.py
@@ -2,10 +2,17 @@
 from fastapi import FastAPI, HTTPException
 from typing import Optional
 from app.schemas import PostCreate,PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
 
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 # first endpoint
