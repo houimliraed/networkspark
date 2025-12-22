b1b3c3029238b3448dd36728d1c33b9cf8726bf6
Raed Houimli <houimli.raed@tek-up.de>
2025-12-22 19:20:42 +0100

    added the tf backend

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -3,12 +3,20 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>6.0"
+      source  = "hashicorp/aws"
+      version = "~>6.0"
     }
+  }
+  backend "s3" {
+    bucket       = "socialmedia-tf-state"
+    key          = "tf-state-prod"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
+
