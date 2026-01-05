c59cf96a5cde11ea6d8c3c19b9eb148b320d4642
Raed Houimli <houimli.raed@tek-up.de>
2026-01-05 02:40:19 +0100

    data az added

--- infra/prod/network.tf
+++ infra/prod/network.tf
@@ -21,60 +21,60 @@ resource "aws_internet_gateway" "main" {
 # public subnets for igw access
 
 resource "aws_subnet" "public_a" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.1.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "${data.aws_region.current.name}-a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
 
-    tags = {
-        Name = "${local.prefix}-public-a"
-    }
+  tags = {
+    Name = "${local.prefix}-public-a"
+  }
 }
 
 resource "aws_route_table" "public_a" {
-    vpc_id = aws_vpc.main.id
-    
-    tags = {
-        Name = "${local.prefix}-public_a"
-    }
-  
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public_a"
+  }
+
 }
 resource "aws_route_table_association" "public_a" {
-    subnet_id = aws_subnet.public_a.id
-    route_table_id = aws_route_table.public_a
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a
 }
 resource "aws_route" "public_internet_access_a" {
-    route_table_id = aws_route_table.public_a.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+  route_table_id         = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 
 resource "aws_subnet" "public_b" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.1.2.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "${data.aws_region.current.name}-b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[1]
 
-    tags = {
-        Name = "${local.prefix}-public-b"
-    }
+  tags = {
+    Name = "${local.prefix}-public-b"
+  }
 }
 
 resource "aws_route_table" "public_b" {
-    vpc_id = aws_vpc.main.id
-    
-    tags = {
-        Name = "${local.prefix}-public_b"
-    }
-  
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public_b"
+  }
+
 }
 resource "aws_route_table_association" "public_b" {
-    subnet_id = aws_subnet.public_b.id
-    route_table_id = aws_route_table.public_b
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b
 }
 resource "aws_route" "public_internet_access_b" {
-    route_table_id = aws_route_table.public_b.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
